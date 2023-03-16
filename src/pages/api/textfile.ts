d992a7368537d6e81785d9c54e731572a9e1bff9
custom3dwebapp <rahul@custom3dweb.com>
2023-03-16 08:48:45 -0400

    server functionalities added

--- src/pages/api/textfile.ts
+++ src/pages/api/textfile.ts
@@ -1,35 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import formidable from "formidable";
 import FormData from "form-data";
+import httpProxyMiddleware from "next-http-proxy-middleware";
 
-const Handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const token = req.headers.authorization?.split(" ")[1];
-
-    if (!token) {
-      res.status(300).json({ message: "Not a valid token" });
-    }
-
-    const folderName = req.query.folderName;
-    console.log(req.body);
-
-    const response = await fetch(
-      `http://3.145.17.29:8443/text?folderName=${folderName}`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        body: req.body,
-      }
-    );
-
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
 
-export default Handler;
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  httpProxyMiddleware(req, res, {
+    target: "http://3.145.17.29:8443/",
+  });
+};
+
+export default handler;
+//`http://3.145.17.29:8443/text?folderName=${folderName}
