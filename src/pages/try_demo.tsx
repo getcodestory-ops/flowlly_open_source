03f4c721ed1d48683a6194eb47120c0a4ffc3481
custom3dwebapp <rahul@custom3dweb.com>
2023-04-25 19:10:33 +0530

    changed language around personal assistant

--- src/pages/try_demo.tsx
+++ src/pages/try_demo.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>Construction Documentation</title>
-        <meta name="description" content="Your personal assistant for construction professionals" />
+        <meta name="description" content="Personal assistant for construction professionals" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
