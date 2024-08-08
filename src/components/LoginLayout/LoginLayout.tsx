c417088fbbd1cf8d46fadd5babb0f8e47dff84f6
Pragya Vaishanav <131640746+vaishanav-at-hypothetic@users.noreply.github.com>
2024-08-08 17:46:28 -0400

    applogin sign up pages are corrected with same background

--- src/components/LoginLayout/LoginLayout.tsx
+++ src/components/LoginLayout/LoginLayout.tsx
@@ -4,35 +4,18 @@ import { useToast } from "@/components/ui/use-toast";
 import { createClient } from "@/utils/supabase/client";
 
 import Link from "next/link";
-import Image from "next/image";
-
-import { Toaster } from "@/components/ui/toaster";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-// const signUp = async (formData: FormData) => {
-//   "use server";
-
-//   const origin = headers().get("origin");
-//   const email = formData.get("email") as string;
-//   const password = formData.get("password") as string;
-//   const supabase = createServerCLient();
-
-//   const { error } = await supabase.auth.signUp({
-//     email,
-//     password,
-//     options: {
-//       emailRedirectTo: `${origin}/auth/callback`,
-//     },
-//   });
-
-//   if (error) {
-//     return redirect("/login?message=Could not authenticate user");
-//   }
-
-//   return redirect("/login?message=Check email to continue sign in process");
-// };
+import { AuthBackground } from "@/components/AuthBackground/AuthBackground";
 
 export const LoginLayout = ({
   signIn,
@@ -70,18 +53,16 @@ export const LoginLayout = ({
     });
   };
   return (
-    <main>
-      <Toaster />
-
-      <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] bg-white">
-        <div className="flex items-center justify-center py-12">
-          <div className="mx-auto grid w-[350px] gap-6">
-            <div className="grid gap-2 text-center">
-              <h1 className="text-3xl font-bold">Login</h1>
-              <p className="text-balance text-muted-foreground">
-                Enter your email below to login to your account
-              </p>
-            </div>
+    <AuthBackground>
+      <div className="flex items-center justify-center h-screen ">
+        <Card className="mx-auto max-w-sm ">
+          <CardHeader>
+            <CardTitle className="text-3xl text-center">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
             <form>
               <div className="grid gap-4">
                 <div className="grid gap-2">
@@ -111,45 +92,25 @@ export const LoginLayout = ({
                     name="password"
                     placeholder="••••••••"
                     required
-                    // onChange={(e) => setPassword(e.target.value)}
-                    // onKeyUp={(e) => {
-                    //   if (e.key === "Enter") {
-                    //     handleLogin(email, password);
-                    //   }
-                    // }}
                   />
                 </div>
-                <Button
-                  type="submit"
-                  className="w-full"
-                  //   onClick={() => handleLogin(email, password)}
-                  formAction={signIn}
-                >
+                <Button type="submit" className="w-full" formAction={signIn}>
                   Login
                 </Button>
                 <Button variant="outline" className="w-full" disabled={true}>
                   Login with Google
                 </Button>
               </div>
+              <div className="mt-4 text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <Link href="signup" className="underline">
+                  Sign up
+                </Link>
+              </div>
             </form>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <Link href="signup" className="underline">
-                Sign up
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="hidden bg-white lg:block  flex content-center xl:mt-32">
-          <Image
-            src="https://upthcaewktgrqjieqiya.supabase.co/storage/v1/object/public/images/hero-v2.c5845c47e7c7d49cf5c1.png"
-            alt="Logo"
-            width="800"
-            height="400"
-            className="mx-auto"
-          />
-        </div>
+          </CardContent>
+        </Card>
       </div>
-    </main>
+    </AuthBackground>
   );
 };
