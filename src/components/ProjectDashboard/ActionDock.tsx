fc847575fb3a6ac8c74ccd84dfbb6ce5988b86b3
Pragya Vaishanav <131640746+vaishanav-at-hypothetic@users.noreply.github.com>
2024-08-11 09:57:03 -0400

    Action dock bug correction

--- src/components/ProjectDashboard/ActionDock.tsx
+++ src/components/ProjectDashboard/ActionDock.tsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 // import { PencilIcon } from "lucide-react";
 
 // import MediaRecorderButton from "../ChatInput/MediaRecorderButton";
@@ -16,39 +16,39 @@
 //   ],
 // };
 
-// export function ActionDock() {
-//   return (
-//     <div className="flex sticky items-center justify-center gap-4">
-//       <div className=" flex  flex-col items-center justify-center overflow-hidden rounded-lg  bg-background ">
-//         <TooltipProvider>
-//           <Dock direction="middle">
-//             {DATA.navbar.map((item) => (
-//               <DockIcon key={item.label}>
-//                 <Tooltip>
-//                   <TooltipTrigger asChild>
-//                     <Link
-//                       href={item.href}
-//                       className={cn(
-//                         buttonVariants({ variant: "ghost", size: "icon" }),
-//                         "size-12 rounded-full"
-//                       )}
-//                     >
-//                       <item.icon className="size-4" />
-//                     </Link>
-//                   </TooltipTrigger>
-//                   <TooltipContent>
-//                     <p>{item.label}</p>
-//                   </TooltipContent>
-//                 </Tooltip>
-//               </DockIcon>
-//             ))}
-//           </Dock>
-//         </TooltipProvider>
-//       </div>
-//       <div className="mt-8">
-//         <Separator orientation="vertical" />
-//         <MediaRecorderButton />
-//       </div>
-//     </div>
-//   );
-// }
+export function ActionDock() {
+  return (
+    <div className="flex sticky items-center justify-center gap-4">
+      {/* <div className=" flex  flex-col items-center justify-center overflow-hidden rounded-lg  bg-background ">
+        <TooltipProvider>
+          <Dock direction="middle">
+            {DATA.navbar.map((item) => (
+              <DockIcon key={item.label}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Link
+                      href={item.href}
+                      className={cn(
+                        buttonVariants({ variant: "ghost", size: "icon" }),
+                        "size-12 rounded-full"
+                      )}
+                    >
+                      <item.icon className="size-4" />
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{item.label}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </DockIcon>
+            ))}
+          </Dock>
+        </TooltipProvider>
+      </div>
+      <div className="mt-8">
+        <Separator orientation="vertical" />
+        <MediaRecorderButton />
+      </div>*/}
+    </div>
+  );
+}
