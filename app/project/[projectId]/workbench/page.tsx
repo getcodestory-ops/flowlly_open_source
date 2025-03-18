606e020740c548cf5f0fe6099199800b34ded3bf
Pragya Vaishanav <40742845+pragyavaishanav@users.noreply.github.com>
2025-03-18 16:51:23 -0400

    Bug/fpd 94/on refresh the chat agent is selected but the url and opened area is workbenc (#166)
    * added use editor store to align the tool bar
    
    * Debugged
    
    * convert to paf moved to utils
    
    * bug resolved

--- app/project/[projectId]/workbench/page.tsx
+++ app/project/[projectId]/workbench/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+import { useEffect } from "react";
 import AssignmentHome from "./components/Assignment";
+import { useStore } from "@/utils/store";
 
 export default function ConstructionDashboardHome() {
+	const { setAppView } = useStore((state) => ({
+		setAppView: state.setAppView,
+	}));
+
+	useEffect(() => {
+		setAppView("workflows");
+	}, []);
 	return <AssignmentHome />;
 }
