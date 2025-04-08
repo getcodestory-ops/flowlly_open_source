726879382a119b90b2f9ba545d9237cbb8c161b5
getcodestory-ops <ops@getcodestory.com>
2025-04-08 18:42:53 -0400

    Added new changes for google search in the chat

--- src/api/eventResourceRoutes.ts
+++ src/api/eventResourceRoutes.ts
@@ -3,7 +3,7 @@ import { Session } from "@supabase/supabase-js";
 import axios from "axios";
 
 
-interface EventResourceEntity {
+interface EventResourceRow {
     id: string;
     row: Record<string, string | number | boolean | null>;
     created_at: string;
@@ -11,8 +11,17 @@ interface EventResourceEntity {
     event_resource_id: string;
 }
 
+interface EventResourceEntity {
+    id: string;
+	created_at: string;
+	event_id: string;
+	metadata: Record<string, string | number | boolean | null>;
+    event_resource_rows: EventResourceRow[];
+}
 
-export const getEventResource = async(session: Session, eventResourceId: string) : Promise<EventResourceEntity[] | null> => {
+
+
+export const getEventResourceRows = async(session: Session, eventResourceId: string) : Promise<EventResourceRow[] | null> => {
 	const url = `${process.env.NEXT_PUBLIC_DEVELOPMENT_SERVER_URL}/resource/event_resource/${eventResourceId}`;
 	try {
 		const response = await axios.get(url, {
@@ -23,6 +32,22 @@ export const getEventResource = async(session: Session, eventResourceId: string)
 		return response.data;
 	} catch (error) {
 		console.error("Error in getEventResource", error);
+		return null;
+	}
+};
+
+
+export const getEventResoucesByEventId = async(session: Session, eventId: string) : Promise<EventResourceEntity[] | null> => {
+	const url = `${process.env.NEXT_PUBLIC_DEVELOPMENT_SERVER_URL}/resource/event_resource/event/${eventId}`;
+	try {
+		const response = await axios.get(url, {
+			headers: {
+				Authorization: `Bearer ${session.access_token}`,
+			},
+		});
+		return response.data;
+	} catch (error) {
+		console.error("Error in getEventResoucesByEventId", error);
 		return null;
 	}
 };
