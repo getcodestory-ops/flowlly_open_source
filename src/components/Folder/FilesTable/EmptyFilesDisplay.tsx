5ab4f536132095de1421c0326b32ffabb7d608fe
getcodestory-ops <ops@getcodestory.com>
2025-06-02 18:02:56 -0400

    added new things

--- src/components/Folder/FilesTable/EmptyFilesDisplay.tsx
+++ src/components/Folder/FilesTable/EmptyFilesDisplay.tsx
@@ -5,9 +5,9 @@ import { TableCell, TableRow } from "@/components/ui/table";
 export const EmptyFilesDisplay: React.FC = () => {
 	return (
 		<TableRow>
-			<TableCell className="text-center" colSpan={4}>
+			<TableCell className="text-center flex flex-col items-center justify-center pt-8" colSpan={4}>
 				<div className="flex flex-col items-center justify-center pt-8">
-					<FileSearch className="w-20 h-20 text-gray-400 mb-4" />
+					<FileSearch className="w-10 h-10 text-gray-400 mb-4" />
 					<p className="text-lg font-medium text-gray-500">No files found</p>
 					<p className="text-sm text-gray-400">
             It looks like there are no files here. Try uploading or checking back
